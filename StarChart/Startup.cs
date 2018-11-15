b6652f2361057276ae4659ece1666bac709a8986
saeedsul <saeed.suleiman@hotmail.co.uk>
2018-11-15 10:57:10 +0000

    Completed the module

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -18,7 +18,8 @@ namespace StarChart
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+        { 
+            // add mvc
             app.UseMvc();
         }
     }
