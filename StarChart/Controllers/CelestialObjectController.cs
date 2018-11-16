1f0dd86f4c79d5b21b1e8cfdb3fab6dc86e9d38f
saeedsul <saeed.suleiman@hotmail.co.uk>
2018-11-16 08:12:56 +0000

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,44 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var model = _context.CelestialObjects.Find(id);
+            if (model == null)            
+                return NotFound();            
+
+            model.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+
+            return Ok(model);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var model = _context.CelestialObjects.Where(x=>x.Name == name).ToList();
+            if(!model.Any())
+                return NotFound();
+
+            foreach(var item in model)
+            {
+                item.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == item.Id).ToList();                
+            }            
+            return Ok(model);
+        }
+
+        public IActionResult GetAll()
+        {
+            var model = _context.CelestialObjects.ToList();
+            if(!model.Any())
+                return NotFound();
+            foreach(var item in model)
+            {
+                item.Satellites = _context.CelestialObjects.Where(x=>x.OrbitedObjectId == item.Id).ToList();
+            }
+            return Ok(model);
         }
     }
 }
