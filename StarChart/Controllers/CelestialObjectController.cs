ebdeeb3ad974eabe6a9e918461af39dad78b0afc
saeedsul <saeed.suleiman@hotmail.co.uk>
2018-11-16 12:21:12 +0000

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -19,8 +20,8 @@ namespace StarChart.Controllers
         public IActionResult GetById(int id)
         {
             var model = _context.CelestialObjects.Find(id);
-            if (model == null)            
-                return NotFound();            
+            if (model == null)
+                return NotFound();
 
             model.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
 
@@ -30,28 +31,77 @@ namespace StarChart.Controllers
         [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
-            var model = _context.CelestialObjects.Where(x=>x.Name == name).ToList();
-            if(!model.Any())
+            var model = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (!model.Any())
                 return NotFound();
 
-            foreach(var item in model)
+            foreach (var item in model)
             {
-                item.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == item.Id).ToList();                
-            }            
+                item.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == item.Id).ToList();
+            }
             return Ok(model);
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var model = _context.CelestialObjects.ToList();
-            if(!model.Any())
+            var record = _context.CelestialObjects.ToList();
+            if (!record.Any())
                 return NotFound();
-            foreach(var item in model)
+            foreach (var item in record)
             {
-                item.Satellites = _context.CelestialObjects.Where(x=>x.OrbitedObjectId == item.Id).ToList();
+                item.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == item.Id).ToList();
             }
-            return Ok(model);
+            return Ok(record);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject celestialObject)
+        {
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject celestialObject)
+        {
+            var record = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (record == null)
+                return NotFound();
+
+            record.Name = celestialObject.Name;
+            record.OrbitalPeriod = celestialObject.OrbitalPeriod;
+            record.OrbitedObjectId = celestialObject.OrbitedObjectId;
+
+            _context.CelestialObjects.Update(record);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var record = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (record == null)
+                return NotFound();
+            record.Name = name;
+            _context.CelestialObjects.Update(record);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var records = _context.CelestialObjects.Where( x=> x.Id == id || x.OrbitedObjectId == id);
+            if(!records.Any())
+                return NotFound();
+            _context.CelestialObjects.RemoveRange(records);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
